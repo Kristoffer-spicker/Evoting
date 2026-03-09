6f5d6d90ea5a6631868b9f979daefb026fbfa254
Mathilde Marcussen <mathilde.marcussen@hotmail.com>
2026-03-09 14:04:59 +0100

    Added the rest of the database tables
    Co-authored-by: Kristoffer <kspi@itu.dk>

--- Database/init.sql
+++ Database/init.sql
@@ -2,3 +2,21 @@ CREATE TABLE IF NOT EXISTS encryptedVotes (
     id        TEXT PRIMARY KEY,
     ballot    TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS extendedVotes (
+    id        TEXT PRIMARY KEY,
+    ballot    TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS reencryptedTriplets (
+    triplet    TEXT PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS decryptedTriplets (
+    triplet     TEXT PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS finalTally (
+    candidate_id  TEXT PRIMARY KEY,
+    vote_count    INT
+);
